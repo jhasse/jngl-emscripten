88a4bef954235973b4fb8ce19631160b1a61b58a
Jan Niklas Hasse <jhasse@bixense.com>
2021-02-07 10:17:18 +0100

    Use jnglInit

--- src/game.cpp
+++ src/game.cpp
@@ -1,37 +1,10 @@
-#ifdef __EMSCRIPTEN__
-#include <emscripten.h>
-#endif
-#ifdef __EMSCRIPTEN__
-
-#include <emscripten.h>
-#include <SDL.h>
-
-#define GL_GLEXT_PROTOTYPES 1
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
-
-#else
-
-#include <SDL2/SDL.h>
-
-#define GL_GLEXT_PROTOTYPES 1
-#include <SDL2/SDL_opengles2.h>
-
-#endif
-
 #include <jngl.hpp>
+#include <jngl/init.hpp>
 
-std::function<void()> loop;
-void main_loop() {
-	loop();
-}
-
-JNGL_MAIN_BEGIN {
-	jngl::showWindow("Tutorial 2 - Printing Text", 800, 600);
-    jngl::setFont("data/Poppins-Bold.ttf");
-
-	loop = [&] {
-		jngl::updateInput();
+class MyWork : public jngl::Work {
+	void step() override {
+	}
+	void draw() const override {
 		jngl::setBackgroundColor(jngl::keyDown('s') ? 0 : 255, 0, 0);
 		jngl::print("Hello World!", -100, -10);
 		// jngl::draw("data/jngl.png", jngl::getMousePos());
@@ -42,14 +15,13 @@ JNGL_MAIN_BEGIN {
 		if (jngl::keyDown('a')) {
 			jngl::debugLn("asdf");
 		}
-		jngl::swapBuffers();
-	};
-#ifdef __EMSCRIPTEN__
-	emscripten_set_main_loop(main_loop, 0, true);
-#else
-	while (jngl::running()) {
-		loop();
 	}
-#endif
+};
+
+std::function<std::shared_ptr<jngl::Work>()> jnglInit(jngl::AppParameters& params) {
+	params.screenSize = { 800, 600 };
+	return []() {
+		jngl::setFont("data/Poppins-Bold.ttf");
+		return std::make_shared<MyWork>();
+	};
 }
-JNGL_MAIN_END
